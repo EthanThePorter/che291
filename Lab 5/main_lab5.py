b87b04b8d9159016cb673721e0b8752b7972859a
Ethan Porter <ethantheporter@outlook.com>
2022-05-23 12:59:22 -0400

    V4 - Latest changes from macbook pro

--- Lab 5/main_lab5.py
+++ Lab 5/main_lab5.py
@@ -9,11 +9,11 @@ import openpyxl
 
 # 3. Import and format data
 data = pd.read_excel('KineticDataFromChE101_vS22.xlsx','Sheet1')
-td = data.iloc[1:142,0]
-CAd = data.iloc[1:142,1]
+td = data.iloc[1:142, 0]
+CAd = data.iloc[1:142, 1]
 
-tr = np.asarray(td, dtype = float)
-CA = np.asarray(CAd, dtype = float)
+tr = np.asarray(td, dtype=float)
+CA = np.asarray(CAd, dtype=float)
 
 
 # 4. Initialize Known Parameters
@@ -34,19 +34,37 @@ for i in range(n - 4):
     trf2[i] = tr[i + 2]
     CAf2[i] = CA[i + 2]
     CBf2[i] = CBo - (CAo - CAf2[i])
-    Rf2[i] = -(-3 * CA[i + 2] + 4 * CA[i + 3] - CA[i + 4]) / 2 / h;
+    Rf2[i] = -(-3 * CA[i + 2] + 4 * CA[i + 3] - CA[i + 4]) / 2 / h
 
 tf2 = (time.perf_counter() - start1) * 1000
 print(tf2)
 
 
 # 6. Calculate reaction rate using forward difference formula (EQN 6b) and array operations
-start1=time.perf_counter()
-trf1=tr[2:-3]
-CAf1=CA[2:-3]
-CBf1=
-Rf1=
-tf1=(time.perf_counter()-start1)*1000
+start1 = time.perf_counter()
+
+trf1 = tr[2:-3]
+CAf1 = CA[2:-3]
+
+CBf1 = CAf1 + CBo - CAo
+
+Rf1 = -(-3 * CAf1 + 4 * CA[3:-2] - CA[4:-1]) / 2 / h
+
+tf1 = (time.perf_counter()-start1)*1000
 print(tf1)
-print(Rf1)
+
+
+# 7. Calculate reaction rate using central difference formula (EQN 8)
+Rate_CDF = (CA[:-2] - CA[2:]) / 2 / h
+
+print(Rf1[:5])
+print(Rate_CDF[:5])
+
+
+
+
+
+
+
+
 
